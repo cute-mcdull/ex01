5dd37117640df0753d5cf9e68d3ae00dddb089a7
unknown <cutemcdull@outlook.com>
2026-06-03 18:03:55 +0800

    v2 增加键盘输入功能

--- hello.c
+++ hello.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main() {
     printf("Hello World\n");
+    char text[100];
+    printf("ÇëÊäÈëÄÚÈİ£º");
+    scanf("%s", text);
+    printf("Êä³ö£º%s\n", text);
+
     return 0;
 }
